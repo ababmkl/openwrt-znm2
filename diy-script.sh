b5ce8226c90a9a94aa1d4e9a1c0fa44767621396
ysuolmai <fan0417@gmail.com>
2025-02-28 04:29:36 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -101,6 +101,7 @@ cd ..
 cp -rf tmp_repo/target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/* $DTS_PATH
 rm -rf tmp_repo
 
+cat $DTS_PATH/ipq6018.dtsi
 
 keywords_to_delete=(
     "xiaomi_ax3600" "xiaomi_ax9000" "xiaomi_ax1800" "glinet" "jdcloud_ax6600"
