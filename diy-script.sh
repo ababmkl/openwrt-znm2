60ba260709b8b5df42656aaf6e4f03389080e177
ysuolmai <fan0417@gmail.com>
2025-02-28 09:56:12 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -102,7 +102,6 @@ cd ..
 cp -rf tmp_repo/target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/* $DTS_PATH
 rm -rf tmp_repo
 
-cat ${DTS_PATH}ipq6018.dtsi
 
 keywords_to_delete=(
     "xiaomi_ax3600" "xiaomi_ax9000" "xiaomi_ax1800" "glinet" "jdcloud_ax6600"
@@ -172,6 +171,10 @@ else
         "CONFIG_PACKAGE_usbutils=y"
     )
 fi
+
+cat ${DTS_PATH}ipq6018-cmiot.dtsi
+
+
 
 [[ $FIRMWARE_TAG == *"EMMC"* ]] && provided_config_lines+=(
     "CONFIG_PACKAGE_luci-app-diskman=y"
